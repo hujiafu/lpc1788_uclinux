8f5530cf070ada70679a40e53e37bad12e0fae3e
root <root@hujiafu-virtual-machine.(none)>
2016-01-30 00:23:15 +0800

    modified:   filesystem/rootfs/initramfs-list-min 	new file:   uclinux_app_test/Makefile 	new file:   uclinux_app_test/fb_test.c 	add application fbtest

---
+++ cortexm_uclinux-master/uclinux_app_test/fb_test.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+
+int main()
+{
+	int fp = 0;
+	struct fb_var_screeninfo vinfo;
+	struct fb_fix_screeninfo finfo;
+	fp = open("/dev/fb0", O_RDWR);
+
+	if(fp < 0){
+		printf("Error: Can not open framebuffer device\n");
+		exit(1);
+	}
+
+	if(ioctl(fp, FBIOGET_FSCREENINFO, &finfo)){
+		printf("Error reading fixed information\n");
+		exit(2);
+	}
+
+	if(ioctl(fp, FBIOGET_VSCREENINFO, &vinfo)){
+		printf("Error reading variable information\n");
+		exit(3);
+	}
+
+	printf("The mem is :%d\n", finfo.smem_len);
+	printf("The line_length is :%d\n", finfo.line_length);
+	printf("The xres is :%d\n", vinfo.xres);
+	printf("The yres is :%d\n", vinfo.yres);
+	printf("bits_per_pixel is :%d\n", vinfo.bits_per_pixel);
+	close(fp);
+}
