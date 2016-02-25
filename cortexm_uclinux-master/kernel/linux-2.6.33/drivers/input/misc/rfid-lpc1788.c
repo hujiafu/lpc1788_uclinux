01c5f91c622db3be960f94dfb698889669735be5
root <root@hujiafu-virtual-machine.(none)>
2016-02-25 08:55:14 +0800

    modified:   drivers/input/misc/rfid-lpc1788.c

--- cortexm_uclinux-master/kernel/linux-2.6.33/drivers/input/misc/rfid-lpc1788.c
+++ cortexm_uclinux-master/kernel/linux-2.6.33/drivers/input/misc/rfid-lpc1788.c
@@ -1,8 +1,32 @@
 
+struct rfid_data {
+        struct rfid_platform_data chip;
+        bool use_smbus;
+
+        /*  
+         * Lock protects against activities from other Linux tasks,
+         * but not from changes by other I2C masters.
+         */
+        struct mutex lock;
+
+        u8 *writebuf;
+        unsigned write_max;
+        unsigned num_addresses;
+
+        /*  
+         * Some chips tie up multiple I2C addresses; dummy devices reserve
+         * them for us, and we'll use them with SMBus calls.
+         */
+        struct i2c_client *client[];
+};
+
+
+
 
 static int rfid_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
 	struct rfid_platform_data chip;
+	struct rfid_data *rfid;
 	int err;
 
 	if (client->dev.platform_data) {
@@ -16,6 +40,14 @@ static int rfid_probe(struct i2c_client *client, const struct i2c_device_id *id)
 		err = -EPFNOSUPPORT;
 		return err;
 	}
+
+	rfid = kzalloc(sizeof(struct rfid_data) +
+                1 * sizeof(struct i2c_client *), GFP_KERNEL);
+        if (!rfid) {
+                err = -ENOMEM;
+                goto err_out;
+        }
+
 
 }
 
